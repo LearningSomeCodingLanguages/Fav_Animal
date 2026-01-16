db80e8618ddd7ab2a006e28d94efc54483033d73
Kevin <kevinalcaraz2023@bmhs.edu.bz>
2026-01-16 13:36:32 -0600

    Update punchline display logic and remove audio

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 let isVisible = false; // Track visibility state
 
 document.getElementById("showPunchline").addEventListener("click", function() {
-  // Play the audio
-  const audio = document.getElementById("calling");
-  audio.play();
 
   // Change HTML
-  document.getElementById("punchline").classList.remove("hidden");
+  document.getElementById("Placeholder").classList.remove("hidden");
 
   document.body.style.backgroundColor = "#ffe4b5";
 
-  document.getElementById("jokeImage").src = "sly.png";
+  document.getElementById("Placeholder").src = "Placeholder.png";
 
-  const punchline = document.getElementById("punchline");
-  punchline.style.transform = "scale(1.5)";
-  punchline.style.color = "red";
+  const Placeholder = document.getElementById("Placeholder");
+  Placeholder.style.transform = "scale(1.5)";
+  Placeholder.style.color = "red";
 
   setTimeout(() => {
-    punchline.style.transform = "scale(1)";
-    punchline.style.color = "#2e8b57";
+    Placeholder.style.transform = "scale(1)";
+    Placeholder.style.color = "#2e8b57";
   }, 1500);
